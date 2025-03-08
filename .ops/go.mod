186d82d2a7b1c04226feb4f18a09ce357ce0c4e8
notHyotic <hy0t1c1@gmail.com>
2025-03-08 16:30:40 -0600

    install ops

---
+++ .ops/go.mod
@@ -0,0 +1,10 @@
+module ops
+
+go 1.22.12
+
+require lesiw.io/ops v0.14.0
+
+require (
+	lesiw.io/defers v0.9.0 // indirect
+	lesiw.io/flag v0.7.0 // indirect
+)
---
+++ .ops/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "lesiw.io/ops"
+
+type Ops struct{}
+
+func main()        { ops.Handle(Ops{}) }
+func (Ops) Hello() { println("Hello world!") }

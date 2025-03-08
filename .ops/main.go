f508d322984802cc17aee30340e7d81c24040a0c
notHyotic <hy0t1c1@gmail.com>
2025-03-08 16:41:36 -0600

    add aws-region, run go fmt

--- .ops/main.go
+++ .ops/main.go
@@ -4,5 +4,9 @@ import "lesiw.io/ops"
 
 type Ops struct{}
 
-func main()        { ops.Handle(Ops{}) }
-func (Ops) Hello() { println("Hello world!") }
+func main() {
+	ops.Handle(Ops{})
+}
+func (Ops) Hello() {
+	println("Hello world!")
+}
